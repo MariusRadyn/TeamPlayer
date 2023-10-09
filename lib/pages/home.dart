4729ff5606396d90b0efde0ad859612dd7a1f322
Marius Radyn <marius.radyn@hidglobal.com>
2023-10-09 16:00:54 +0200

    new files

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,38 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:scrollable_reorderable_navbar/scrollable_reorderable_navbar.dart';
 import 'package:team_player/bars/bottomNavBarHome.dart';
+import 'package:circle_list/circle_list.dart';
+import 'package:rive/rive.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
+  List<String> icons = [
+    'images/add.png',
+    'images/Alarm1.jpg',
+    'images/ArrowDown.png',
+    'images/ArrowUp.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0x313131),
       bottomNavigationBar: BottomNavBarHome(),
-      body: SafeArea(
-        child: Row(
-          children : <Widget> [
-            TextButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/page1');
-              },
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.grey[300],
+      body: Center(
+        child: CircleList(
+            origin: Offset(0,0),
+            children: icons.map((imgPaths) {
+              return GestureDetector(
+                onTap: (){ },
+                child: Image.asset(imgPaths, scale: 2,),
+              );
+            }).toList(),
+          centerWidget: GestureDetector(
+            onTap: () {},
+            child: ClipRRect(
+              borderRadius:BorderRadius.all(Radius.circular(100)),
+              child: RiveAnimation.asset('assets/power.riv') ,
               ),
-              icon: Icon(Icons.sync),
-              label: Text('Label'),
             ),
-          ],
+          ),
         ),
-      ),
     );
   }
 
