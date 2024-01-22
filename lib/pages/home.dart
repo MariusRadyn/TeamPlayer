3fad6b7a062c503c0e1515121e850b9c466a1210
Marius Radyn <marius.radyn@hidglobal.com>
2024-01-22 15:31:59 +0200

    Working on home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:circle_list/circle_list.dart';
+import 'package:path/path.dart';
 import 'package:team_player/pages/playlist_page.dart';
 import 'package:team_player/pages/profile_page.dart';
 import 'package:team_player/pages/settings_page.dart';
@@ -30,68 +31,154 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      //backgroundColor: Colors.white,
-      //bottomNavigationBar: BottomNavBarHome(),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                   image: DecorationImage(
-                    image: AssetImage("images/pearl_black_crop.png"),
-                    fit: BoxFit.cover,
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/pearl_black_crop.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Container(
+
+            child: Stack(
+                children: [
+
+                  // Backdrop
+                  Container(
+                    alignment: Alignment.topCenter,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        stops: [
+                          0.3,
+                          0.9,
+                        ],
+                        colors: [
+                          Colors.teal,
+                          Colors.black26,
+                        ]
+                      ),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(100),
+                        bottomRight: Radius.circular(100),
+                      ),
+                    ),
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
+
+                  // White Container
+                  Container(
+                    margin: EdgeInsets.only(
+                        top: 90,
+                        left: 10,
+                        right: 10
+                    ),
+                    height: 150,
+                    decoration: const BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  ),
+
+                  // Avatar
+                  Container(
+                    margin: EdgeInsets.only(top: 35),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 55,
+                          backgroundColor: Colors.white,
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/author1.jpg") ,
+                            radius: 50,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Container(
-                          height: 80,
-                          color: Colors.blueGrey,
-                          child: Center(
-                            child: CircleAvatar(
-                              backgroundImage: AssetImage("images/author1.jpg") ,
-                              radius: 50,
-                            ),
+                      Container(
+                        padding: EdgeInsets.only(top:155),
+                        child: Text('Welcom Marius',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       )
                     ],
                   ),
-                ),
-                // child: Center(
-                //   child: CircleList(
-                //       origin: Offset(0,0),
-                //     centerWidget: GestureDetector(
-                //       onTap: () => exit(0),
-                //       child: ClipRRect(
-                //         borderRadius:BorderRadius.all(Radius.circular(10)),
-                //         child: Image.asset('images/power2.png'),
-                //         ),
-                //       ),
-                //       children: myPageRoutes.keys.map((item) {
-                //         return GestureDetector(
-                //           onTap: () {
-                //             Navigator.push(context,
-                //               MaterialPageRoute(builder: (context) => myPageRoutes[item]!),
-                //             );
-                //           },
-                //           child: Image.asset(item,width: 50,height: 50),
-                //         );
-                //       }).toList(),
-                //     ),
-                //   ),
-              ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                            top:190,
+                        ),
+                        width: 120,
+                        height: 40,
+                        decoration: const BoxDecoration(
+                            color: Colors.deepOrangeAccent,
+                            borderRadius: BorderRadius.all(Radius.circular(20))
+                        ),
+                        child: TextButton(
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          onPressed: (){
+                            _butLogin();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
             ),
-          ],
-        ),
-      ),
+          )
+          //},
+          ),
+          // child: Center(
+          //   child: CircleList(
+          //       origin: Offset(0,0),
+          //     centerWidget: GestureDetector(
+          //       onTap: () => exit(0),
+          //       child: ClipRRect(
+          //         borderRadius:BorderRadius.all(Radius.circular(10)),
+          //         child: Image.asset('images/power2.png'),
+          //         ),
+          //       ),
+          //       children: myPageRoutes.keys.map((item) {
+          //         return GestureDetector(
+          //           onTap: () {
+          //             Navigator.push(context,
+          //               MaterialPageRoute(builder: (context) => myPageRoutes[item]!),
+          //             );
+          //           },
+          //           child: Image.asset(item,width: 50,height: 50),
+          //         );
+          //       }).toList(),
+          //     ),
+          //   ),
+        //),
+      //),
     );
   }
+}
+
+void _butLogin(){
+
 }
